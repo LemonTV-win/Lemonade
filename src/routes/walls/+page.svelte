dcc85d29e989e9692af77ad0871d3f6b6cb69cd6
mkpoli <git@mkpo.li>
2025-06-13 02:00:36 +0900

    feat: add Ocarnus minimap

---
+++ src/routes/walls/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	const MAP_SIZE = {
+		x: 500,
+		y: 500
+	};
+</script>
+
+<svg width={MAP_SIZE.x} height={MAP_SIZE.y}>
+	<image xlink:href="/minimaps/ocarnus.png" x="0" y="0" width={MAP_SIZE.x} height={MAP_SIZE.y} />
+</svg>
