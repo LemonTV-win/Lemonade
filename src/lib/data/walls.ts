432664179f91932218571712f63a66e265fc0de7
mkpoli <git@mkpo.li>
2025-06-13 08:24:45 +0900

    feat: add Windy Town walls

--- src/lib/data/walls.ts
+++ src/lib/data/walls.ts
@@ -19,67 +19,115 @@ export interface Wall {
 }
 
 export let WALLS: Record<string, Wall> = {
-	A_SITE_PLANT: {
-		map: 'ocarnus',
-		name: 'A Site Plant',
-		direction: 'vertical',
-		ultimate: false,
-		transparent: false,
-		position: {
-			start: { x: 59.95, y: 56 },
-			angle: -50 * (Math.PI / 180)
+	...{
+		// Ocarnus
+		A_SITE_PLANT: {
+			map: 'ocarnus',
+			name: 'A Site Plant',
+			direction: 'vertical',
+			ultimate: false,
+			transparent: false,
+			position: {
+				start: { x: 59.95, y: 56 },
+				angle: -50 * (Math.PI / 180)
+			},
+			deploy_position: {
+				x: 59,
+				y: 56
+			},
+			images: {
+				deploy: '/walls/wall_1_deploy.jpg',
+				overview: '/walls/wall_1_overview.jpg'
+			},
+			jump: 'none'
 		},
-		deploy_position: {
-			x: 59,
-			y: 56
+		MID_TO_A: {
+			map: 'ocarnus',
+			name: 'Mid to A',
+			direction: 'vertical',
+			ultimate: false,
+			transparent: false,
+			position: {
+				start: { x: 44.25, y: 55.5 },
+				angle: -62 * (Math.PI / 180)
+			},
+			deploy_position: {
+				x: 41.53,
+				y: 59.72
+			},
+			images: {
+				deploy: '/walls/wall_2_deploy.jpg',
+				overview: '/walls/wall_2_overview.jpg',
+				end: '/walls/wall_2_end.jpg'
+			},
+			video: '/walls/wall_2_video.mp4',
+			jump: 'none'
 		},
-		images: {
-			deploy: '/walls/wall_1_deploy.jpg',
-			overview: '/walls/wall_1_overview.jpg'
-		},
-		jump: 'none'
+		A_SITE_ATTACK: {
+			map: 'ocarnus',
+			name: 'A Site Attack',
+			direction: 'vertical',
+			ultimate: false,
+			transparent: false,
+			position: {
+				start: { x: 60.5, y: 52.5 },
+				angle: -58 * (Math.PI / 180)
+			},
+			deploy_position: {
+				x: 59,
+				y: 56
+			},
+			images: {
+				deploy: '/walls/wall_3_deploy.jpg',
+				overview: '/walls/wall_3_overview.jpg',
+				end: '/walls/wall_3_end.jpg'
+			},
+			jump: 'twice'
+		}
 	},
-	MID_TO_A: {
-		map: 'ocarnus',
-		name: 'Mid to A',
-		direction: 'vertical',
-		ultimate: false,
-		transparent: false,
-		position: {
-			start: { x: 44.25, y: 55.5 },
-			angle: -62 * (Math.PI / 180)
+	...{
+		// Windy Town
+		WT_A_PLANT: {
+			map: 'windy_town',
+			name: 'A Site Plant',
+			direction: 'vertical',
+			ultimate: false,
+			transparent: false,
+			position: {
+				start: { x: 59.06, y: 45.38 },
+				angle: -50 * (Math.PI / 180)
+			},
+			deploy_position: {
+				x: 59.54,
+				y: 44.38
+			},
+			images: {
+				deploy: '/walls/wall_wt_a_plant_deploy.jpg',
+				overview: '/walls/wall_wt_a_plant_overview.jpg',
+				end: '/walls/wall_wt_a_plant_end.jpg'
+			},
+			jump: 'none'
 		},
-		deploy_position: {
-			x: 41.53,
-			y: 59.72
-		},
-		images: {
-			deploy: '/walls/wall_2_deploy.jpg',
-			overview: '/walls/wall_2_overview.jpg',
-			end: '/walls/wall_2_end.jpg'
-		},
-		video: '/walls/wall_2_video.mp4',
-		jump: 'none'
-	},
-	A_SITE_ATTACK: {
-		map: 'ocarnus',
-		name: 'A Site Attack',
-		direction: 'vertical',
-		ultimate: false,
-		transparent: false,
-		position: {
-			start: { x: 60.5, y: 52.5 },
-			angle: -58 * (Math.PI / 180)
-		},
-		deploy_position: {
-			x: 59,
-			y: 56
-		},
-		images: {
-			deploy: '/walls/wall_3_deploy.jpg',
-			overview: '/walls/wall_3_overview.jpg',
-			end: '/walls/wall_3_end.jpg'
-		},
-		jump: 'twice'
+		WT_A_ULT: {
+			map: 'windy_town',
+			name: 'A Site Ultimate Plant',
+			direction: 'vertical',
+			ultimate: true,
+			transparent: false,
+			position: {
+				start: { x: 59.06, y: 45.38 },
+				angle: -45 * (Math.PI / 180)
+			},
+			deploy_position: {
+				x: 59.54,
+				y: 44.38
+			},
+			images: {
+				deploy: '/walls/wall_wt_a_ult_deploy.jpg',
+				overview: '/walls/wall_wt_a_ult_overview.jpg',
+				end: '/walls/wall_wt_a_ult_end.jpg'
+			},
+			jump: 'none'
+		}
 	}
 };
