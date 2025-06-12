7d477ce773dadbe1e8130b17b91a3091a389efec
mkpoli <git@mkpo.li>
2025-06-13 02:02:54 +0900

    feat: add example wall

--- src/routes/walls/+page.svelte
+++ src/routes/walls/+page.svelte
@@ -1,10 +1,32 @@
-<script>
+<script lang="ts">
 	const MAP_SIZE = {
 		x: 500,
 		y: 500
 	};
+
+	let WALLS = {
+		WALL_1: {
+			images: {
+				deploy: '/walls/wall_1_deploy.jpg',
+				overview: '/walls/wall_1_overview.jpg'
+			}
+		}
+	};
 </script>
 
-<svg width={MAP_SIZE.x} height={MAP_SIZE.y}>
-	<image xlink:href="/minimaps/ocarnus.png" x="0" y="0" width={MAP_SIZE.x} height={MAP_SIZE.y} />
-</svg>
+<main>
+	<svg width={MAP_SIZE.x} height={MAP_SIZE.y}>
+		<image xlink:href="/minimaps/ocarnus.png" x="0" y="0" width={MAP_SIZE.x} height={MAP_SIZE.y} />
+	</svg>
+	<div>
+		{#each Object.entries(WALLS) as [key, wall]}
+			<div>
+				<h2>{key}</h2>
+
+				<img src={wall.images.deploy} alt={`${key} deploy`} width={500} />
+
+				<img src={wall.images.overview} alt={`${key} overview`} width={500} />
+			</div>
+		{/each}
+	</div>
+</main>
