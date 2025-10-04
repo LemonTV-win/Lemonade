57a55b16ef7d90b4ec750de5129e9e8b2ffecbe0
mkpoli <git@mkpo.li>
2025-10-04 17:23:01 +0900

    refactor: self-host map previews

--- src/lib/components/MapIcon.svelte
+++ src/lib/components/MapIcon.svelte
@@ -1,26 +1,27 @@
 <script lang="ts">
 	import type { GameMap } from '$lib/data/game';
 
-	const MAP_2_IMAGE: Record<string, string> = {
-		base_404:
-			'https://klbq-web-cms.strinova.com/prod/strinova_web/images/202411/830d991d-24ce-4c00-92e9-2b4eb5ff703c.jpg',
-		area_88:
-			'https://klbq-web-cms.strinova.com/prod/strinova_web/images/202411/24c1ac4e-40d0-4383-a060-15bb59db183e.jpg',
-		port_euler:
-			'https://klbq-web-cms.strinova.com/prod/strinova_web/images/202411/e46ea826-5ff4-465a-b575-fe5f29e02cd5.jpg',
-		windy_town:
-			'https://klbq-web-cms.strinova.com/prod/strinova_web/images/202411/47755586-4bf9-465d-9e88-566c70fab0bc.jpg',
-		space_lab:
-			'https://klbq-web-cms.strinova.com/prod/strinova_web/images/202411/be6f044e-0ace-4b85-89f5-0c8710b9c1fd.jpg',
-		cauchy_district:
-			'https://klbq-web-cms.strinova.com/prod/strinova_web/images/202411/f704a413-6e0b-476b-bec5-a442a890079e.jpg',
-		cosmite:
-			'https://klbq-web-cms.strinova.com/prod/strinova_web/images/202411/c0951e88-691f-4698-8c27-e65ab25ff166.jpg',
-		ocarnus:
-			'https://static.wikitide.net/strinovawiki/thumb/9/9d/Intro_Ocarnus.png/450px-Intro_Ocarnus.png'
+	import Base404 from '$lib/assets/maps/base_404.jpg';
+	import Area88 from '$lib/assets/maps/area_88.jpg';
+	import PortEuler from '$lib/assets/maps/port_euler.jpg';
+	import WindyTown from '$lib/assets/maps/windy_town.jpg';
+	import SpaceLab from '$lib/assets/maps/space_lab.jpg';
+	import CauchyDistrict from '$lib/assets/maps/cauchy_district.jpg';
+	import Cosmite from '$lib/assets/maps/cosmite.jpg';
+	import Ocarnus from '$lib/assets/maps/ocarnus.png';
+
+	const MAP_2_IMAGE: Record<GameMap, string> = {
+		base_404: Base404,
+		area_88: Area88,
+		port_euler: PortEuler,
+		windy_town: WindyTown,
+		space_lab: SpaceLab,
+		cauchy_district: CauchyDistrict,
+		cosmite: Cosmite,
+		ocarnus: Ocarnus
 	};
 
-	const MAP_2_NAME: Record<string, string> = {
+	const MAP_2_NAME: Record<GameMap, string> = {
 		base_404: 'Base 404',
 		area_88: 'Area 88',
 		port_euler: 'Port Euler',
@@ -31,7 +32,7 @@
 		ocarnus: 'Orcanus'
 	};
 
-	let { map }: { map: string } = $props();
+	let { map }: { map: GameMap } = $props();
 </script>
 
 <img src={MAP_2_IMAGE[map]} alt={MAP_2_NAME[map]} class="h-6 w-10 rounded object-cover" />
